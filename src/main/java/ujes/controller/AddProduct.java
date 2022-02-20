0af8f1f8114d65f12437135f25cb6d302763253d
Afarrhana_ <91364758+Afarrhana@users.noreply.github.com>
2022-02-21 07:06:33 +0800

    Update AddProduct.java

--- src/main/java/ujes/controller/AddProduct.java
+++ src/main/java/ujes/controller/AddProduct.java
@@ -1,4 +1,4 @@
-package oracle.controller;
+package ujes.controller;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -6,7 +6,6 @@ import java.io.InputStream;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +15,6 @@ import javax.servlet.http.Part;
 import ujes.dao.ProductDAO;
 import ujes.model.Product;
 
-@MultipartConfig
 @WebServlet("/AddProduct")
 public class AddProduct extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -27,31 +25,6 @@ public class AddProduct extends HttpServlet {
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//*************
-				System.out.println("In do post method of Add Image servlet");
-				Part file=request.getPart("pPics");
-				
-				String imageFileName=file.getSubmittedFileName(); //get selected image file name
-				System.out.println("Selected Image File Name: " + imageFileName);
-				
-				String uploadPath="C:/Users/user/eclipse-workspace/0000 UJES SYSTEM/src/main/webapp/images/"+imageFileName; //upload path where we have to upload our actual image
-				System.out.println("Upload Path: " + uploadPath);
-				
-				//uploading our selected image into images folder
-				
-				try {
-					FileOutputStream fos=new FileOutputStream(uploadPath);
-					InputStream is=file.getInputStream();
-					
-					byte[] data= new byte[is.available()];
-					is.read(data);
-					fos.write(data);
-					fos.close();
-				}
-				catch(Exception e) {
-					e.printStackTrace();
-				}
-				//*************
 		
 		Product p = new Product();
 		p.setcID(Integer.parseInt(request.getParameter("cID")));
@@ -59,7 +32,7 @@ public class AddProduct extends HttpServlet {
 		p.setpDesc(request.getParameter("pDesc"));
 		p.setpPrice(Double.parseDouble(request.getParameter("pPrice")));
 		p.setpQty(Integer.parseInt(request.getParameter("pQty")));
-		p.setpPics(imageFileName);
+		p.setpPics(request.getParameter("pPics"));
 		
 		dao.addProduct(p);
 		dao.addBridgeProduct(p);
