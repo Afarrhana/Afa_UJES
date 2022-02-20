c1e95e574f98e03cb0fc53fe8f4d2a99f5f7f64e
Afarrhana_ <91364758+Afarrhana@users.noreply.github.com>
2022-02-21 06:51:56 +0800

    Create LoginSServlet.java

---
+++ src/main/java/ujes/controller/LoginSServlet.java
@@ -0,0 +1,84 @@
+package ujes.controller;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginBServlet
+ */
+@WebServlet("/LoginSServlet")
+public class LoginSServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /** 
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginSServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		PrintWriter out = response.getWriter();
+		out.print("<!DOCTYPE html><html><body>");
+		out.print("Username:" + email + "<br>");
+		out.print("Password:"+ password + "<br>");
+		
+		out.print("</body></html>");
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+            
+            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","db_gryffindor","system");
+            Statement st = conn.createStatement();
+           
+            ResultSet sql = st.executeQuery("SELECT * FROM seller WHERE SEMAIL = '" + email + "' AND SPASSW = '" + password + "'");
+            if(sql.next()) {
+            	response.sendRedirect("homeS.jsp");
+            }
+            else {
+            	out.println("Wrong username/password");
+            	//response.sendRedirect("loginBuyer.jsp"); //patut ke buat new page? utk login kalau dia ada error, sbb ni nnti dia direct je hahaha
+            }
+            
+        }
+		
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+        } 
+		
+		catch (SQLException e) {
+            e.printStackTrace();
+        }
+	}
+
+}
