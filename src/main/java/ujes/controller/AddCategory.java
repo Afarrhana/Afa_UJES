25a5e1f0ee16289e4f8cec36571b0dc1f417a75d
Afarrhana_ <91364758+Afarrhana@users.noreply.github.com>
2022-02-21 07:07:09 +0800

    Update AddCategory.java

--- src/main/java/ujes/controller/AddCategory.java
+++ src/main/java/ujes/controller/AddCategory.java
@@ -1,4 +1,4 @@
-package oracle.controller;
+package ujes.controller;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -28,35 +28,10 @@ public class AddCategory extends HttpServlet {
 	 }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//*************
-		System.out.println("In do post method of Add Image servlet");
-		Part file=request.getPart("cPics");
-		
-		String imageFileName=file.getSubmittedFileName(); //get selected image file name
-		System.out.println("Selected Image File Name: " + imageFileName);
-		
-		String uploadPath="C:/Users/user/eclipse-workspace/0000 UJES SYSTEM/src/main/webapp/images/"+imageFileName; //upload path where we have to upload our actual image
-		System.out.println("Upload Path: " + uploadPath);
-		
-		//uploading our selected image into images folder
-		
-		try {
-			FileOutputStream fos=new FileOutputStream(uploadPath);
-			InputStream is=file.getInputStream();
-			
-			byte[] data= new byte[is.available()];
-			is.read(data);
-			fos.write(data);
-			fos.close();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		//*************
 		
 		Category d = new Category();
 		d.setcName(request.getParameter("cName"));
-		d.setcPics(imageFileName);
+		d.setcPics(request.getParameter("cPics"));
 		System.out.println("MASUK SERVLET"+d.getcName()+d.getcPics());
 		dao.addCategory(d);
 		
