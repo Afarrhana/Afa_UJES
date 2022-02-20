eb45bd980c55d34dd616d6115000e537b075dce7
Afarrhana_ <91364758+Afarrhana@users.noreply.github.com>
2022-02-21 07:00:08 +0800

    Update UpdateCategory.java

--- src/main/java/ujes/controller/UpdateCategory.java
+++ src/main/java/ujes/controller/UpdateCategory.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +15,6 @@ import javax.servlet.http.Part;
 import ujes.dao.CategoryDAO;
 import ujes.model.Category;
 
-@MultipartConfig
 @WebServlet("/UpdateCategory")
 public class UpdateCategory extends HttpServlet {
 	private CategoryDAO dao;
@@ -34,36 +32,10 @@ public class UpdateCategory extends HttpServlet {
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//*************
-				System.out.println("In do post method of Add Image servlet");
-				Part file=request.getPart("cPics");
-				
-				String imageFileName=file.getSubmittedFileName(); //get selected image file name
-				System.out.println("Selected Image File Name: " + imageFileName);
-				
-				String uploadPath="C:/Users/user/eclipse-workspace/0000 UJES SYSTEM/src/main/webapp/images/"+imageFileName; //upload path where we have to upload our actual image
-				System.out.println("Upload Path: " + uploadPath);
-				
-				//uploading our selected image into images folder
-				
-				try {
-					FileOutputStream fos=new FileOutputStream(uploadPath);
-					InputStream is=file.getInputStream();
-					
-					byte[] data= new byte[is.available()];
-					is.read(data);
-					fos.write(data);
-					fos.close();
-				}
-				catch(Exception e) {
-					e.printStackTrace();
-				}
-				//*************
-		
 		Category d = new Category();
 		d.setcID(Integer.parseInt(request.getParameter("cID")));
 		d.setcName(request.getParameter("cName"));
-		d.setcPics(imageFileName);
+		d.setcPics("cPics");
 		System.out.println(d.getcID() + d.getcName() + d.getcPics());
 		
 		dao.updateCategory(d);
