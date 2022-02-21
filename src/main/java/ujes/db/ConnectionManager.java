40cdc6ac39be5bb719aafab96d0735b3aeddab68
Afarrhana_ <91364758+Afarrhana@users.noreply.github.com>
2022-02-21 14:57:46 +0800

    Update ConnectionManager.java

--- src/main/java/ujes/db/ConnectionManager.java
+++ src/main/java/ujes/db/ConnectionManager.java
@@ -5,10 +5,10 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 	static Connection con;
-	private static final String DB_DRIVER= "org.postgresql.Driver";
-	private static final String DB_CONNECTION= "jdbc:postgresql://ec2-54-209-221-231.compute-1.amazonaws.com:5432/de2ejfdl93uqof";
-	private static final String DB_USER= "ihawdciqkgdspx";
-	private static final String DB_PASSWORD= "d0a32ddb478ecf1390eb0b275d3ed8278804864c252d4b8c6eb2faebd733a514";
+	private static final String DB_DRIVER= "com.mysql.jdbc.Driver";
+	private static final String DB_CONNECTION= "jdbc:mysql://us-cdbr-east-05.cleardb.net/heroku_c52d31833f8842d";
+	private static final String DB_USER= "b2fcdfacffe805";
+	private static final String DB_PASSWORD= "67b805ee";
 	
 	public static Connection getConnection() {
 		// TODO Auto-generated method stub
