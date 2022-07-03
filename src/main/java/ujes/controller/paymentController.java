135c98799105389c7b93abc072aac67d1dc22f7b
11318903 <75016327+11318903@users.noreply.github.com>
2022-07-03 19:27:19 +0800

    Update paymentController.java

--- src/main/java/ujes/controller/paymentController.java
+++ src/main/java/ujes/controller/paymentController.java
@@ -33,7 +33,6 @@ public class paymentController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
 		
 		int odID = Integer.parseInt(request.getParameter("oID"));
 		//int byID = Integer.parseInt(request.getParameter("bID"));
